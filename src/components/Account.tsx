0585b01e7e714966e5c97cb7f57b80cf9dd78b3c
Stephen Gordon <n00202054@iadt.ie>
2024-01-10 11:28:16 +0000

    added sendTx

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,13 +1,51 @@
 import { useAccount, useEnsName } from 'wagmi'
 
+
+import {
+	usePrepareSendUserOperation,
+	useSendUserOperation,
+} from "@zerodev/wagmi";
+import { useWaitForTransaction } from "wagmi";
+
 export function Account() {
   const { address } = useAccount()
   const { data: ensName } = useEnsName({ address })
 
+  
+  const sendTx = async () => {
+		try {
+			
+      console.log("sendTx");  
+			const { config } = usePrepareSendUserOperation({
+				to: "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045 ",
+				data: "0x",
+				value: "0.00002",
+			});
+
+			const { sendUserOperation, data } = useSendUserOperation(config);
+
+			
+        sendUserOperation(); 
+
+			// Wait on the status of the tx
+			useWaitForTransaction({
+				hash: data?.hash,
+				enabled: !!data,
+				onSuccess(data) {
+					console.log("Transaction was successful.");
+				},
+			});
+		} catch (error) {}
+	};
+   
+
+
   return (
-    <div>
-      {ensName ?? address}
-      {ensName ? ` (${address})` : null}
-    </div>
-  )
+		<div>
+			{ensName ?? address}
+			{ensName ? ` (${address})` : null}
+			<button onClick={sendTx}>Send</button>
+      
+		</div>
+	);
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,62 +1,67 @@
-import type { AppProps } from 'next/app'
-import NextHead from 'next/head'
-import * as React from 'react'
-import { WagmiConfig, configureChains, createConfig } from 'wagmi'
-import { goerli, mainnet } from 'wagmi/chains'
-import { 
-  GoogleSocialWalletConnector, 
-  FacebookSocialWalletConnector, 
-  GithubSocialWalletConnector,
-  DiscordSocialWalletConnector,
-  TwitchSocialWalletConnector,
-  TwitterSocialWalletConnector,
-} from '@zerodev/wagmi'
+//dashboard.zerodev.app/
+//github.com/orgs/zerodevapp/repositories
+//github.com/zerodevapp/zerodev-privy-nextjs-example
 
-import { publicProvider } from 'wagmi/providers/public'
+import type { AppProps } from "next/app";
+import NextHead from "next/head";
+import * as React from "react";
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { goerli, mainnet } from "wagmi/chains";
+import {
+	GoogleSocialWalletConnector,
+	FacebookSocialWalletConnector,
+	GithubSocialWalletConnector,
+	DiscordSocialWalletConnector,
+	TwitchSocialWalletConnector,
+	TwitterSocialWalletConnector,
+} from "@zerodev/wagmi";
 
+import { publicProvider } from "wagmi/providers/public";
 
 export const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
-  [
-    publicProvider(),
-  ],
-)
+	[mainnet, ...(process.env.NODE_ENV === "development" ? [goerli] : [])],
+	[publicProvider()]
+);
 
 function App({ Component, pageProps }: AppProps) {
-  const [config] = React.useState(() => {
-    if (typeof window !== 'undefined') {
-      const options = { chains, options: { projectId: 'b5486fa4-e3d9-450b-8428-646e757c10f6', shimDisconnect: true } } 
-      return (
-        createConfig({
-          autoConnect: true,
-          connectors: [
-            new GoogleSocialWalletConnector(options),
-            new FacebookSocialWalletConnector(options),
-            new GithubSocialWalletConnector(options),
-            new DiscordSocialWalletConnector(options),
-            new TwitchSocialWalletConnector(options),
-            new TwitterSocialWalletConnector(options),
-          ],
-          publicClient,
-          webSocketPublicClient,
-        })
-      )
-    }
-  })
+	const [config] = React.useState(() => {
+		if (typeof window !== "undefined") {
+			const options = {
+				chains,
+				options: {
+					projectId: "ef3522fe-b6d7-4d79-abe0-2529274958d8",
+					shimDisconnect: true,
+				},
+			};
+			return createConfig({
+				autoConnect: true,
+				connectors: [
+					new GoogleSocialWalletConnector(options),
+					new FacebookSocialWalletConnector(options),
+					new GithubSocialWalletConnector(options),
+					new DiscordSocialWalletConnector(options),
+					new TwitchSocialWalletConnector(options),
+					new TwitterSocialWalletConnector(options),
+				],
+				publicClient,
+				webSocketPublicClient,
+			});
+		}
+	});
 
-  const [mounted, setMounted] = React.useState(false)
-  React.useEffect(() => setMounted(true), [])
-  if (typeof window === 'undefined') return null
+	const [mounted, setMounted] = React.useState(false);
+	React.useEffect(() => setMounted(true), []);
+	if (typeof window === "undefined") return null;
 
-  return (
-    <WagmiConfig config={config!}>
-      <NextHead>
-        <title>wagmi</title>
-      </NextHead>
+	return (
+		<WagmiConfig config={config!}>
+			<NextHead>
+				<title>wagmi</title>
+			</NextHead>
 
-      {mounted && <Component {...pageProps} />}
-    </WagmiConfig>
-  )
+			{mounted && <Component {...pageProps} />}
+		</WagmiConfig>
+	);
 }
 
-export default App
+export default App;
