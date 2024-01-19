e6f71b07d1694c1c33b0b6a6c7a26790b22b1231
Stephen Gordon <n00202054@iadt.ie>
2024-01-19 16:21:09 +0000

    added alchemy sdk to get tx history

--- src/pages/send/index.tsx
+++ src/pages/send/index.tsx
@@ -1,15 +1,41 @@
 // Next
 import { useRouter } from "next/router";
+import { Alchemy, Network } from "alchemy-sdk";
+
+import { useAccount } from "wagmi";
+
+
+const config = {
+	apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
+	network: Network.MATIC_MUMBAI,
+};
+const alchemy = new Alchemy(config);
+
+
 
 export default function Page() {
 
+	const { address } = useAccount();
+
+	const getData = async () => {
+		const data = await alchemy.core.getAssetTransfers({
+			fromBlock: "0x0",
+			fromAddress: address,
+			category: ["internal"],
+		});
+		console.log(data);
+	}
+	
+
+
     const router = useRouter();
-		const { address } = router.query;
+		//const { address } = router.query;
 
     return (
 			<>
 				<h1>Send Page</h1>
 				<p>Route address: {address}</p>
+				<button onClick={getData} >get data</button>
 			</>
 		);
 }
