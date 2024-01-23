8c412e0905b490ea8e752afbe7954040b8f3eeec
Stephen Gordon <n00202054@iadt.ie>
2024-01-23 15:49:00 +0000

    Cleaned up SendUsdc component code

--- src/components/SendUsdc/SendUsdc.tsx
+++ src/components/SendUsdc/SendUsdc.tsx
@@ -1,96 +1,37 @@
-import { useContractWrite, usePrepareContractWrite, erc20ABI } from 'wagmi'
-import { useContractBatchWrite, usePrepareContractBatchWrite } from '@zerodev/wagmi';
-import { parseEther, parseUnits } from "viem";
+// Wagmi
+import { erc20ABI, useWaitForTransaction, useAccount } from "wagmi";
 
+// ZeroDev
 import { usePrepareSendUserOperation, useSendUserOperation } from "@zerodev/wagmi";
-import { useWaitForTransaction, useAccount } from "wagmi";
 
-import { encodeFunctionData, parseAbi } from 'viem';
+// Viem
+import { encodeFunctionData, parseUnits } from "viem";
 
-//const contractABI = parseAbi();
 export default function SendUsdc() {
 
-	const address = "0xc8C26Ab40fe4723519fE66B8dBb625FC070A982c";
-
-	const usdc = "0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97"
-/* 
-	const { config } = usePrepareContractBatchWrite({
-		calls: [
-			{
-				address: usdc,
-				abi: erc20ABI,
-				functionName: "approve",
-				args: ['0xc8C26Ab40fe4723519fE66B8dBb625FC070A982c', parseUnits('100', 6)],
-			}, {
-				address: usdc,
-				abi: erc20ABI,
-				functionName: "transfer",
-				args: ['0xc8C26Ab40fe4723519fE66B8dBb625FC070A982c', parseUnits('100', 6)]
-			}
-		],
-		enabled: true
-	})
-
-	const { sendUserOperation: batchSend, isLoading, data } = useContractBatchWrite(config)
-
-	useWaitForTransaction({
-		hash: data ? data.hash : undefined,
-		enabled: !!data,
-		onSuccess() {
-			console.log("Transaction was successful.")
-		},
-		onError() {
-			alert("Transaction was unsuccessful.")
-		}
-	}) */
-
-	// write contract
-/* 
-	const sendUsdc = async () => {
-		try {
-			// Send the tx
-			if (batchSend) {
-				console.log("sending")
-				console.log("sending")
-				console.log(data, batchSend)
-				batchSend()
-				console.log("sent")
-			} else {
-				console.log("not sending")
-			}
-		} catch (error) {
-			console.log(error)
-		}
-	}
- */
-	const encoded = encodeFunctionData({
-		abi: [
-			{
-				constant: false,
-				inputs: [
-					{ name: "_to", type: "address" },
-					{ name: "_value", type: "uint256" },
-				],
-				name: "transfer",
-				outputs: [{ name: "success", type: "bool" }],
-				stateMutability: "nonpayable",
-				type: "function",
-			},
-		],
-		functionName: "transfer",
-		args: [address, parseUnits("1", 6)],
-	});
-
-	console.log(encoded)
+	// Get the user's address hook
+	const { address } = useAccount();
 	
+	// USDC contract address
+	const usdc = "0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97"
 
+	// Encode the data with Viem Function
+	// Requires the abi of the contract, the function name, and the arguments address and amount
+	const encoded = encodeFunctionData({
+		abi: erc20ABI,
+		functionName: "transfer",
+		args: [!address ? "0xc8C26Ab40fe4723519fE66B8dBb625FC070A982c" : address, parseUnits("1", 6)],
+	});	
+
+	// Prepare the transaction with Wagmi
 	const { config } = usePrepareSendUserOperation({
 		to: usdc,
 		data: encoded,
-		//value: parseEther("0.01"),
 	});
+	// Send the transaction with Wagmi
 	const { sendUserOperation, data } = useSendUserOperation(config);
 
+	// Wait for the transaction executed
 	useWaitForTransaction({
 		hash: data?.hash,
 		enabled: !!data,
@@ -99,28 +40,22 @@ export default function SendUsdc() {
 		},
 	});
 
+	// Send transaction function
 	const sendTx = async () => {
 		try {
-			// Send the tx
-			if (sendUserOperation) {
-				console.log("true");
-				console.log(data, sendUserOperation);
-				sendUserOperation();
-				console.log(data, sendUserOperation);
-			} else {
-				console.log("false");
-			}
+			// check if the operation exists
+			sendUserOperation && sendUserOperation();
 		} catch (error) {
 			console.log(error);
 		}
 	};
-
+	
 	return (
-		<div>
 			<button
+				className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
 				onClick={() => sendTx()}
-			>Send USDC New</button>
-		
-		</div>
+			>
+				Send USDC
+			</button>
 	);
 }
