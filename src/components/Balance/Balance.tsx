844c13fcb55c44a3a067cf4db5575708ee69b464
Stephen Gordon <n00202054@iadt.ie>
2024-01-23 16:40:51 +0000

    balance now gets usdc balance

--- src/components/Balance/Balance.tsx
+++ src/components/Balance/Balance.tsx
@@ -6,9 +6,11 @@ export default function Balance () {
 	const { address } = useAccount();
 
     const result = useBalance({
-        address: address,
-    });
-		console.log(result);
+			address: address,
+			token: "0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97",
+		});
+
+		console.log("balance", result);
     
     return (
             <p>{result?.data?.formatted}</p>
